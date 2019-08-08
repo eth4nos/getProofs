9bf5318326fb70b3bd8bf705b37b7ef4cfd9b08c
Luke Park <twodude@naver.com>
2019-08-08 15:00:43 +0900

    Update tmp.js

--- db/tmp.js
+++ db/tmp.js
@@ -13,42 +13,60 @@ let endBlockNumber = Number(process.argv[3]);
     try {
         for (let i = startBlockNumber; i <= endBlockNumber; i++) {
             let blockNumber = await web3.eth.getBlockNumber();
-        
+
             if (blockNumber + startBlockNumber == i) {
                 let block = await findOne('blocks', { number: i });
+                console.log(block)
 
                 console.log("current block number", blockNumber);
-                console.log("iter block number", i-startBlockNumber);
-                let txNumber = (block.to).length;
+                console.log("iter block number", i - startBlockNumber);
+
+                let toNumber = (block.to).length;
+                let fromNumber = (block.from).length;
+                let txNumber = toNumber > fromNumber ? toNumber : fromNumber;
+
                 for (let j = 0; j < txNumber; j++) {
                     // let from = (block.from)[j], fromIndex = (await findOne('accounts', { address: from })).number;
                     // let to = (block.to)[j], toIndex = (await findOne('accounts', { address: to })).number;
-                    let to = (block.to)[j]
                     // console.log(fromIndex, toIndex);
 
                     web3.eth.getAccounts().then(accounts => {
                         var password = "1234"
 
+                        let to = (block.to)[j];
+                        if (to == undefined) {
+                            // to = (block.to)[0];
+                            to = accounts[0];
+                        }
+                        let delegatedFrom = (block.from)[j];
+                        if (delegatedFrom == undefined) {
+                            // from = (block.from)[j];
+                            delegatedFrom = accounts[0];
+                        }
+                        let from = accounts[0];
+
                         // var fromAddress = accounts[fromIndex];
-                        var fromAddress = accounts[0];
                         // var toAddress = accounts[toIndex];
+                        var fromAddress = from;
                         var toAddress = to;
 
-                        web3.eth.personal.unlockAccount(fromAddress, password, 10).then(() => {
+                        web3.eth.personal.unlockAccount(fromAddress, password, 3).then(() => {
                             web3.eth.sendTransaction({
                                 from: fromAddress,
                                 to: toAddress,
                                 value: 1,
-                                gas: 21000,
+                                gas: 2100000,
+                                data: delegatedFrom
                             }, function (err, hash) {
-                                console.log("> txHash   : ", hash);
+                                // console.log("> txHash: ", hash, "delegatedFrom: ", delegatedFrom, "to: ", toAddress, "from: ", from);
+                                console.log("> txHash: ", hash);
                             });
                         });
                     });
                 }
             }
             else {
-		// wait for mining
+                // wait for mining
                 i--;
                 //console.log("The current block number does not match the expected block number.");
                 //console.log("Pending...")
