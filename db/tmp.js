f971e8cc3d9cf6ee87596741ca9e8e01e68b49aa
jmlee <jmlee@altair.snu.ac.kr>
2019-08-14 12:07:06 +0900

    add comments and delete useless comments

--- db/tmp.js
+++ db/tmp.js
@@ -20,6 +20,7 @@ const promisify = (inner) => {
     })
 }
 
+// send transaction to ethereum
 function sendTx(from, to, value, gas, delegatedFrom) {
     return new Promise(async (resolve, reject) => {
         await web3.eth.sendTransaction({
@@ -30,7 +31,7 @@ function sendTx(from, to, value, gas, delegatedFrom) {
             data: delegatedFrom
         })
         .on('transactionHash', function(hash){
-            console.log('hash', hash);
+            //console.log('hash', hash);
             resolve(hash);
         })
         .on('receipt', function(receipt){
@@ -51,117 +52,36 @@ function sendTx(from, to, value, gas, delegatedFrom) {
     await promisify(() => web3.eth.personal.unlockAccount(from, password, 0));
 
     for (let i = startBlockNumber; i <= endBlockNumber; i++) {
+
+        // get etheruem's current block number
         let blockNumber = await promisify(() => web3.eth.getBlockNumber());
 
         if (blockNumber + startBlockNumber == i) {
 
-            // Check storage size every epoch before sweeping
+            // Check storage size at every epoch
             if (blockNumber % SIZE_CHECK_EPOCH == 0) {
                 exec('printf \"' + blockNumber + '	\" >> storageSize')
                 exec('du -sc ' + PATH + ' | cut -f1 | head -n 1 >> storageSize')
             }
 
+            // get transactions to send from mongodb
             let transactions = await findMany('transactions_test', { blockNum: i });
-            console.log("(current / iter)", blockNumber, "/", i - startBlockNumber);
-
             let txNumber = (transactions != undefined ? transactions.length : 0);
             
-            
-            for (let j = 0; j < txNumber; j++) {
-                console.log(j);
-                // web3.eth.getAccounts().then(accounts => {
+            console.log("ethereum block number:", blockNumber, "/ actual block number:", i - 1);
 
+            for (let j = 0; j < txNumber; j++) {
 
                 let to = (transactions[j]).to;
                 let delegatedFrom = (transactions[j]).from;
 
-                console.log(to, delegatedFrom);
-
-                // web3.eth.personal.unlockAccount(from, password, 10).then(() => {                          
                 let hash = await sendTx(from, to, 1, 2100000, delegatedFrom);
-                
                 console.log("> txHash", j, ":", hash);
-                // }, function (err, hash) {
-                //     // console.log("> txHash: ", hash, "delegatedFrom: ", delegatedFrom, "to: ", toAddress, "from: ", from);
-                //     console.log("> txHash", j, ":", hash);
-                // });
-                // });
-                //});
             }
         }
         else {
-            // wait for mining
+            // just wait for mining
             i--;
-            //console.log("The current block number does not match the expected block number.");
-            //console.log("Pending...")
-            //console.log("iter block number", i-startBlockNumber);
         }
     }
 })();
-// web3.eth.getAccounts().then(accounts => {
-//     var password = "1234"
-//     let from = accounts[0];
-
-//     web3.eth.personal.unlockAccount(from, password, 0).then(() => {
-//         (async () => {
-//             // try {
-//                 for (let i = startBlockNumber; i <= endBlockNumber; i++) {
-//                     let blockNumber = await web3.eth.getBlockNumber();
-
-                    
-
-//                     if (blockNumber + startBlockNumber == i) {
-
-//                         // Check storage size every epoch before sweeping
-//                         if (blockNumber % SIZE_CHECK_EPOCH == 0) {
-//                             exec('printf \"' + blockNumber + '	\" >> storageSize')
-//                             exec('du -sc ' + PATH + ' | cut -f1 | head -n 1 >> storageSize')
-//                         }
-
-//                         let transactions = await findMany('transactions_test', { blockNum: i });
-//                         console.log("(current / iter)", blockNumber, "/", i - startBlockNumber);
-
-//                         let txNumber = (transactions != undefined ? transactions.length : 0);
-                        
-                        
-//                         for (let j = 0; j < txNumber; j++) {
-//                             // web3.eth.getAccounts().then(accounts => {
-
-
-//                             let to = (transactions[j]).to;
-//                             let delegatedFrom = (transactions[j]).from;
-
-
-//                             // web3.eth.personal.unlockAccount(from, password, 10).then(() => {                          
-//                             let hash = await web3.eth.sendTransaction({
-//                                 from: from,
-//                                 to: to,
-//                                 value: 1,
-//                                 gas: 2100000,
-//                                 data: delegatedFrom
-//                             });
-//                             console.log("> txHash", j, ":", hash);
-//                             // }, function (err, hash) {
-//                             //     // console.log("> txHash: ", hash, "delegatedFrom: ", delegatedFrom, "to: ", toAddress, "from: ", from);
-//                             //     console.log("> txHash", j, ":", hash);
-//                             // });
-//                             // });
-//                             //});
-//                         }
-//                     }
-//                     else {
-//                         // wait for mining
-//                         i--;
-//                         //console.log("The current block number does not match the expected block number.");
-//                         //console.log("Pending...")
-//                         //console.log("iter block number", i-startBlockNumber);
-//                     }
-
-//                     // console.log(i, endBlockNumber);
-//                 }
-//             // } catch (err) {
-//             //     return console.error(err);
-//             // }
-//         })();
-//     });
-// });
